fbd88f99a973d535d593965f2e0d47e544a4419b
Hermetheus <allanmolson@gmail.com>
2020-05-25 11:56:11 -0500

    Holy Crap, Serializing Data/Model Views. A lot to take in

---
+++ SolarCoffee.Web/Serialization/OrderMapper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SolarCoffee.Data.Models;
+using SolarCoffee.Web.ViewModels;
+
+namespace SolarCoffee.Web.Serialization
+{
+    /// <summary>
+    /// Handles mapping order data models to and from related View Models
+    /// </summary>
+    public static class OrderMapper
+    {
+        /// <summary>
+        /// Map an Invoicemodel view model to a SalesOrder data Model
+        /// </summary>
+        /// <param name="invoice"></param>
+        /// <returns></returns>
+        public static SalesOrder SerializeInvoiceToOrder(InvoiceModel invoice)
+        {
+            var salesOrderItems = invoice.LineItems
+                .Select(item => new SalesOrderItem()
+                {
+                    Id = item.Id,
+                    Quantity = item.Quantity,
+                    Product = ProductMapper.SerializeProductModel(item.Product)
+                }).ToList();
+
+            return new SalesOrder()
+            {
+                SalesOrderItems = salesOrderItems,
+                CreatedOn = DateTime.UtcNow,
+                UpdatedOn = DateTime.UtcNow,
+            };
+        }
+
+        /// <summary>
+        /// Maps a collection of salesorder to Ordermodels
+        /// </summary>
+        /// <param name="orders"></param>
+        /// <returns></returns>
+        public static List<OrderModel> SerializingOrdersToViewModels(IEnumerable<SalesOrder> orders)
+        {
+            return orders.Select(order => new OrderModel
+            {
+                Id = order.Id,
+                CreatedOn = DateTime.UtcNow,
+                UpdatedOn = DateTime.UtcNow,
+                SalesOrderItems = SerializeSalesOrderItems(order.SalesOrderItems),
+                Customer = CustomerMapper.SerializeCustomer(order.Customer),
+                isPaid = order.IsPaid
+            }).ToList();
+        }
+
+        /// <summary>
+        /// Maps a collection of SalesOrderItems to SalesOrderItemModel
+        /// </summary>
+        /// <param name="orderItems"></param>
+        /// <returns></returns>
+        private static List<SalesOrderItemModel> SerializeSalesOrderItems(IEnumerable<SalesOrderItem> orderItems)
+        {
+            return orderItems.Select(item => new SalesOrderItemModel()
+            {
+                Id = item.Id,
+                Quantity = item.Quantity,
+                Product = ProductMapper.SerializeProductModel(item.Product)
+            }).ToList();
+        }
+    }
+}
